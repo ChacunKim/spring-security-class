0956bcde1f4caee145108ce3180a7f29e81f23d1
ChacunKim <cocodata12@gmail.com>
2023-03-25 00:51:03 +0900

    feat: mission -> add login account, remember-me

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,10 +1,15 @@
 package com.prgrms.devcourse.configures;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /*
 * EnableWebSecurity: WebSecurityConfigurerAdapter 를 상속하고
@@ -16,6 +21,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+/*
+  PasswordEncoder
+    - PasswordEncoder가 없으면 UnmappedIdPasswordEncoder 오류를 호출하게 된다. 즉, spring security는 password에 대한 encoder가 반드시 필요하다.
+    - SpringSecurity 5 부터는 DelegatingPasswordEncoder 가 default로 사용된다.
+    - DelegatingPasswordEncoder: 각각의 암호화 알고리즘 별로 각각의 password encoder를 지정하고 있음(Map<String, PasswordEncoder>)
+    - 즉, Spring Security는 DelegatingPasswordEncoder를 기본적으로 생성하고, 각각 알고리즘 별로 Map<String, PasswordEncoder> 형태로 제공하고 있다
+    - String을 기반으로 PasswordEncoder를 매핑하므로 문자열 앞에 어떤 알고리즘을 사용할 것인지 지정하면 된다.
+    - 알고리즘은 암호화할 문자 앞에 {}형식으로 prefix를 지정해주면 된다.
+      >>>> .password("{noop}user123") 형식으로 {} 안에 사용할 알고리즘의 이름을 적어주면 된다.
+    - PasswordEncoderFactories.createDelegatingPasswordEncoder() 에서 알고리즘 매핑 형식과 사용 가능한 알고리즘들을 볼 수 있다.
+
+    - prefix{} 부분이 생략되는 경우 기본 PasswordEncoder로 bcrypt가 사용됨
+    - password 해시 알고리즘을 변경하거나, 강력한 해시 알고리즘을 사용하여 password를 업그레이드 할 수 있도록 함
+      >> InMemoryUserDetailsManager 객체를 사용한다면 (정확하게는 UserDetailsPasswordService) 최초 로그인 1회 성공 시,
+      {noop} 타입에서 -> {bcrypt} 타입으로 PasswordEncoder가 변경됨
+
+    - PasswordEncoder는 인터페이스
+    - PasswordEncoder의 구현체는 여러 개가 있음
+    - DelegatingPasswordEncoder 는 그 중 하나.
+    - PasswordEncoder 인터페이스 자체가 upgradeEncoding 이라는 메소드를 가지고있기 때문에
+    꼭 DelegatingPasswordEncoder를 사용해야지만 upgradeEncoding을 사용할 수 있는 것은 아니다.
+
+
+    */
+
+    auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}user123").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}admin123").roles("ADMIN");
+  }
 
   @Override
   public void configure(WebSecurity web) {
@@ -42,15 +80,25 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         - logout(): 로그아웃 기능 세부설정
         - rememberMe(): 자동 로그인 기능 세부설정
     */
-    http
-      .authorizeRequests()
-        .antMatchers("/me").hasAnyRole("USER", "ADMIN") // "/me"에 접근하려면 사용자가 USER 또는 ADMIN이라는 접근 권한을 가져야 한다. >> "/me"는 인증영역임.
-        .anyRequest().permitAll() // (그 외에 모든 영역은) 익명영역임. 모두 허용한다.
+    http.authorizeRequests()
+            .antMatchers("/me")
+            .hasAnyRole("USER", "ADMIN") // "/me"에 접근하려면 사용자가 USER 또는 ADMIN이라는 접근 권한을 가져야 한다. >> "/me"는 인증영역임.
+            .anyRequest().permitAll()
         .and()
-      .formLogin()
-        .defaultSuccessUrl("/")
-        .permitAll() // 모두 허용
+            .formLogin()
+            .defaultSuccessUrl("/")
+            .permitAll() // 모두 허용
         .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) //logout 생성자에서 default logout url이 logout이므로 생략 가능
+            .logoutSuccessUrl("/")
+            .invalidateHttpSession(true) //default가 true. logout이 성공했을 때 해당 사용자의 session을 invalidate
+            .clearAuthentication(true)  // default가 true. logout 성공 시 security authentication을 null로 clear
+        .and()
+            .rememberMe()
+            .rememberMeParameter("remember-me")
+            .tokenValiditySeconds(60 * 5)
+            .alwaysRemember(false)
     ;
   }
   /*
